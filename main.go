745a0aa91d2aa835a19096ccb05e14775821524a
tegar97 <eategar@gmail.com>
2025-05-12 17:52:02 +0700

    ok

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // HelloWorldHandler handles requests to the root endpoint
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "test webhook")
+	fmt.Fprintf(w, "test webhook paling baru")
 }
 
 // HelloHandler handles requests to the /hello endpoint
