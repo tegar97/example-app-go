20580803365281be4506238655b40d72bd9c44ab
tegar97 <eategar@gmail.com>
2025-05-12 11:10:15 +0700

    asdasd

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 
 // HelloHandler handles requests to the /hello endpoint
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World 1929322s")
+	fmt.Fprintf(w, "Hello World 19293222s")
 }
 
 func main() {
