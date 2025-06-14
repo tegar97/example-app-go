4075b3780265d2a24fcb89452cdadf2947e58cf7
tegar97 <eategar@gmail.com>
2025-06-15 01:46:49 +0700

    oke

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Environment Value: %s, Timestamp: %s", envValue, timestamp)
 
 	// evv debug_mode value gets
-	fmt.Fprintf(w, "baru banget")
+	fmt.Fprintf(w, "baru ban2get")
 	debugMode := os.Getenv("DEBUG_MODE")
 	fmt.Fprintf(w, "Debug Mode: %s", debugMode)
 
